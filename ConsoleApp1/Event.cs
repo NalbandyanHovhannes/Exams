c640259a3596c9fdb7f8793ce6594a61b632bb09
NalbandyanHovhannes <nalbandyan.hovh@gmail.com>
2021-12-05 12:21:33 +0400

    Add project files.

---
+++ ConsoleApp1/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate void DelEventHandler();
+    class Event
+    {
+
+        public static event DelEventHandler add;
+
+        static void Main(string[] args)
+        {
+            add += new DelEventHandler(USA);
+            add += new DelEventHandler(India);
+            add += new DelEventHandler(England);
+            add.Invoke();
+
+            Console.ReadLine();
+        }
+        static void USA()
+        {
+            Console.WriteLine("USA");
+        }
+
+        static void India()
+        {
+            Console.WriteLine("India");
+        }
+
+        static void England()
+        {
+            Console.WriteLine("England");
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    // Delegate Definition  
+    public delegate int operation(int x, int y);
+
+    public class Program
+    {
+        // Method that is passes as an Argument  
+        // It has same signature as Delegates   
+       public static int Addition(int a, int b)
+
+        {
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+            // Delegate instantiation  
+            operation obj = new operation(Program.Addition);
+
+            // output  
+            Console.WriteLine("Addition is={0}", obj(23, 27));
+            Console.ReadLine();
+        }
+    }
+}
+
+
